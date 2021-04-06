4c74d7604cb7d40726f46de3f6b3e979d36ceb04
EdwinWalela <edwinwalela@gmail.com>
2021-04-06 12:19:39 +0300

    initial commit

---
+++ lib/src/App.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import './screens/Menu.dart';
+
+class App extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Menu(),
+    );
+  }
+}
