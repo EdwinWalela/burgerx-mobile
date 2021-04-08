7d22b102aed9df7292dde34ee1eb78ff8f978747
EdwinWalela <edwinwalela@gmail.com>
2021-04-08 12:56:34 +0300

    item felx

--- lib/src/widgets/item.dart
+++ lib/src/widgets/item.dart
@@ -20,6 +20,7 @@ class Item extends StatelessWidget {
           )
         ],
       ),
+      padding: EdgeInsets.all(10.0),
       margin: EdgeInsets.all(10.0),
       child: Row(
         children: [
@@ -33,37 +34,53 @@ class Item extends StatelessWidget {
   }
 
   Widget thumbnail(String thumb) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20.0),
-      child: Image.network(
-        thumb,
-        width: 160,
-        height: 160,
+    return Expanded(
+      flex: 1,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: Image.network(
+          thumb,
+        ),
       ),
     );
   }
 
   Widget detailsColumn(FoodItem item) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(margin: EdgeInsets.only(bottom: 10.0)),
-        title(item.name),
-        Container(margin: EdgeInsets.only(bottom: 5.0)),
-        stars(item.stars),
-        Container(margin: EdgeInsets.only(bottom: 5.0)),
-        price(item.price),
-        Container(margin: EdgeInsets.only(bottom: 5.0)),
-        addToCartBtn(),
-      ],
+    return Expanded(
+      flex: 1,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(margin: EdgeInsets.only(bottom: 10.0)),
+          title(item.name),
+          Container(margin: EdgeInsets.only(bottom: 5.0)),
+          description(item.ingredients),
+          Container(margin: EdgeInsets.only(bottom: 5.0)),
+          stars(item.stars),
+          Container(margin: EdgeInsets.only(bottom: 5.0)),
+          price(item.price),
+          Container(margin: EdgeInsets.only(bottom: 5.0)),
+          addToCartBtn(),
+        ],
+      ),
     );
   }
 
-  Widget addToCartBtn() {
-    return ElevatedButton.icon(
-      onPressed: () {},
-      icon: Icon(Icons.shopping_bag_outlined),
-      label: Text('Add to Cart'),
+  Widget title(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontSize: 20.0,
+      ),
+    );
+  }
+
+  Widget description(String desc) {
+    return Text(
+      desc,
+      style: TextStyle(
+        fontSize: 15.0,
+      ),
     );
   }
 
@@ -86,12 +103,11 @@ class Item extends StatelessWidget {
     );
   }
 
-  Widget title(String title) {
-    return Text(
-      title,
-      style: TextStyle(
-        fontSize: 20.0,
-      ),
+  Widget addToCartBtn() {
+    return ElevatedButton.icon(
+      onPressed: () {},
+      icon: Icon(Icons.shopping_bag_outlined),
+      label: Text('Add to Cart'),
     );
   }
 }
