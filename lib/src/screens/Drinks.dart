f78acbdf56a3b3a3922a1a639008fa2d4f93218f
EdwinWalela <edwinwalela@gmail.com>
2021-04-09 11:27:08 +0300

    drinks sample data

--- lib/src/screens/Drinks.dart
+++ lib/src/screens/Drinks.dart
@@ -6,10 +6,10 @@ class Drinks extends StatelessWidget {
   Widget build(BuildContext context) {
     //TODO: Get Items from bloc
     List<FoodItem> _items = [
-      FoodItem('https://bit.ly/3wBNaN5', 'Coke', 100, 0, 'd'),
-      FoodItem('https://bit.ly/3wBNaN5', 'Coke', 100, 0, 'd'),
-      FoodItem('https://bit.ly/3wBNaN5', 'Coke', 100, 0, 'd'),
-      FoodItem('https://bit.ly/3wBNaN5', 'Coke', 100, 0, 'd'),
+      FoodItem('https://bit.ly/3wBNaN5', 'Coke', 100, 0, '300ml'),
+      FoodItem('https://bit.ly/3wBNaN5', 'Coke', 100, 0, '500ml'),
+      FoodItem('https://bit.ly/3wBNaN5', 'Coke', 100, 0, '1 Liter'),
+      FoodItem('https://bit.ly/3wBNaN5', 'Coke', 100, 0, '250ml'),
     ];
     return Scaffold(
       body: ItemList(_items),
