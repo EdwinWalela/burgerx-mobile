1c6976c0c46f80aaa1f0bb828c7c6f8073f696d3
EdwinWalela <edwinwalela@gmail.com>
2021-04-12 10:30:46 +0300

    menu api provider

---
+++ lib/src/resources/menu_api_provider.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+import '../models/Food_Item.dart';
+
+class MenuAPIProvider {
+  Client client = Client();
+  String _baseURL = 'http://localhost:3000/api';
+
+  Future<List<FoodItem>> fetchMenu() async {
+    var url = Uri.parse('$_baseURL/menu');
+
+    final response = await client.get(url);
+    final menu = List.from(json.decode(response.body));
+
+    return menu.cast<FoodItem>();
+  }
+
+  Future<List<FoodItem>> fetchMenuCategory(String category) async {
+    var url = Uri.parse('$_baseURL/menu/$category');
+
+    final response = await client.get(url);
+
+    final menu = List.from(json.decode(response.body));
+
+    return menu.cast<FoodItem>();
+  }
+}
