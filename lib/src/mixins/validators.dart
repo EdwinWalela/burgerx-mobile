bcc05e4f10187116ed5964b5adefdbbce23552e6
EdwinWalela <edwinwalela@gmail.com>
2021-05-04 10:53:31 +0300

    email validator

--- lib/src/mixins/validators.dart
+++ lib/src/mixins/validators.dart
@@ -3,7 +3,9 @@ import 'dart:async';
 class Validators {
   final validateEmail =
       StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
-    if (email.contains('@')) {
+    if (email.contains('@') &&
+        email.contains('.') &&
+        email.split('.')[1].length > 1) {
       sink.add(email);
     } else {
       sink.addError("Invalid Email");
