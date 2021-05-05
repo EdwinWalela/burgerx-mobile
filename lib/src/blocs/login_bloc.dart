1b4f7e00b3bdf6cdfb5f4bcdde3bc1e997270a3c
EdwinWalela <edwinwalela@gmail.com>
2021-05-05 10:16:55 +0300

    login bloc

---
+++ lib/src/blocs/login_bloc.dart
@@ -0,0 +1,25 @@
+import '../models/User.dart';
+import 'package:rxdart/rxdart.dart';
+import '../mixins/validators.dart';
+import '../resources/repository.dart';
+import '../resources/auth_api_provider.dart';
+
+class LoginBloc extends Validators {
+  final _repository = Repository();
+
+  // StreamControllers
+  final _email = BehaviorSubject<String>();
+  final _password = BehaviorSubject<String>();
+
+  // Getters
+  Function(String) get changeEmail => _email.sink.add;
+  Stream<String> get email => _email.stream.transform(validateEmail);
+
+  Function(String) get changePasswrod => _password.sink.add;
+  Stream<String> get password => _password.stream.transform(validatePassword);
+
+  dispose() {
+    _email.close();
+    _password.close();
+  }
+}
