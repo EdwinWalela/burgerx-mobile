082f725554b34e3260d0e4395a83cdfb8338fb70
EdwinWalela <edwinwalela@gmail.com>
2021-05-05 10:33:17 +0300

    auth provider authentication

--- lib/src/resources/auth_api_provider.dart
+++ lib/src/resources/auth_api_provider.dart
@@ -9,11 +9,30 @@ class AuthAPIProvider {
   Future<int> registerUser(User user) async {
     var url = Uri.parse('$_baseURL/register');
 
-    final response = await client.post(url, body: {
-      'email': user.email,
-      'password': user.password,
-      'username': user.username,
-    });
+    final response = await client.post(
+      url,
+      body: {
+        'email': user.email,
+        'password': user.password,
+        'username': user.username,
+      },
+    );
+
+    return response.statusCode;
+  }
+
+  Future<int> authUser(User user) async {
+    var url = Uri.parse('$_baseURL/login');
+
+    final response = await client.post(
+      url,
+      body: {
+        'email': user.email,
+        'password': user.password,
+      },
+    );
+
+    // if 200 store user+token in local db
 
     return response.statusCode;
   }
