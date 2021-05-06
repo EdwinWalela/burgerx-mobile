215c1b64cdbf6d78cfca454f6aa546ba543198ea
EdwinWalela <edwinwalela@gmail.com>
2021-05-06 12:12:29 +0300

    global bloc provider

---
+++ lib/src/blocs/global_bloc_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import './global_bloc.dart';
+
+class GlobalBlocProvider extends InheritedWidget {
+  bool updateShouldNotify(_) => true;
+
+  final bloc = GlobalBloc();
+
+  GlobalBlocProvider({Key key, Widget child}) : super(key: key, child: child);
+
+  static GlobalBloc of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<GlobalBlocProvider>())
+        .bloc;
+  }
+}
