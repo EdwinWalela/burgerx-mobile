41f6cddf10607d99619e004160758fd2bb7dc36d
EdwinWalela <edwinwalela@gmail.com>
2021-05-06 10:58:07 +0300

    db provider init

---
+++ lib/src/resources/db_provider.dart
@@ -0,0 +1,35 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/User.dart';
+
+class DbProvider {
+  Database db;
+
+  init() async {
+    Directory docsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(docsDirectory.path, "burgerx.db");
+
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute(
+          """
+            CREATE TABLE users
+            (
+              id INTEGER PRIMARY KEY,
+              username VARCHAR(255),
+              email VARCHAR(255),
+              address VARCHAR(255),
+              mobile VARCHAR(255),
+              token VARCHAR(255)
+            )
+          """,
+        );
+      },
+    );
+  }
+}
