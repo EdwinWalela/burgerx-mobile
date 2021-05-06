2d14c1372d27d76f3f1abc5d89f56bb5b977a452
EdwinWalela <edwinwalela@gmail.com>
2021-05-06 11:51:49 +0300

    loading screen

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -2,14 +2,15 @@ import 'package:burgers/src/screens/Login.dart';
 import 'package:flutter/material.dart';
 // import './screens/MenuTab/MenuTab.dart';
 // import './screens/Login.dart';
-import './screens/Register.dart';
+// import './screens/Register.dart';
+import './screens/Loading.dart';
 
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // home: RegisterScreen(),
-      home: LoginScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        // home: RegisterScreen(),
+        // home: LoginScreen(),
+        home: Loading());
   }
 }
